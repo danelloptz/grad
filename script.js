1a0d5a44f82d17d029f88915fc4989f33bd9d4f7
reza--boy <60574856+danelloptz@users.noreply.github.com>
2024-02-09 23:04:45 +0300

    Added full worked and tested hello block

--- script.js
+++ script.js
@@ -9,3 +9,38 @@ $(window).on('scroll click', function() {
         }
     });
 });
+
+// open and close burger menu
+$('.nav_burger_wrapper').click(function() {
+    if ($('.nav_burger').attr('data-check') == 0) {
+        $('.nav_burger').addClass('open').removeClass('close').attr('data-check', '1');
+        $('.nav_burger_wrapper').css('animation', 'NavWrapperRotate .8s forwards');
+        $('.burger').css( {
+            'animation': 'BurgerShow .7s forwards',
+            'display': 'flex'
+        });
+    } else {
+        $('.nav_burger').removeClass('open').addClass('close').attr('data-check', '0');
+        $('.nav_burger_wrapper').css('animation', 'NavWrapperRotateClose .8s forwards');
+        $('.burger').css('animation', 'BurgerClose .7s forwards');
+        setTimeout(() => $('.burger').css('display', 'none'), 800);
+    }
+});
+$('.burger_link').click(function() {
+    const itemLink = '.' + $(this).attr('data-link');
+    document.querySelector(itemLink).scrollIntoView({behavior: "smooth"});
+    $('.nav_burger').removeClass('open').addClass('close').attr('data-check', '0');
+    $('.nav_burger_wrapper').css('animation', 'NavWrapperRotateClose .8s forwards');
+    $('.burger').css('animation', 'BurgerClose .7s forwards');
+    setTimeout(() => $('.burger').css('display', 'none'), 800);
+});
+
+// Подчёркивание активного элемента меню в хедере
+
+let header_items = document.querySelectorAll(".header_link");
+header_items.forEach(item => {
+    item.addEventListener("click", () => {
+        document.querySelector('.header_link_active').classList.remove('header_link_active');
+        item.classList.add('header_link_active');
+    });
+})
